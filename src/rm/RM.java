c4e63994728531c5425ed7327a49e2901e87126c
kehao96 <kehaohu1996@gmail.com>
2020-11-19 17:10:53 -0500

    update rm

--- src/rm/RM.java
+++ src/rm/RM.java
@@ -1,6 +1,7 @@
 package rm;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,12 +11,17 @@ import java.util.List;
 public class RM {
     private static int portNumber;
     private static List<String> registerServers;
+    private static final String NEW_ADD = "new server added";
+    private static List<Integer> serverPorts;
+
     static {
         portNumber = 7000;
         registerServers = new ArrayList<>();
     }
     public static void main(String[] args) throws IOException{
-
+        for (String arg: args) {
+            serverPorts.add(Integer.parseInt(arg));
+        }
         ServerSocket ss = new ServerSocket(portNumber);
         System.out.println("RM: " + registerServers.size()+" member");
         while(true){
@@ -40,6 +46,9 @@ public class RM {
                         registerServers.add(server);
                     }
                 }
+                for (int port: serverPorts) {
+                    new ServerThread(port).start();
+                }
                 System.out.println("RM: "+registerServers.size()+" member:"+message);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -47,5 +56,33 @@ public class RM {
             }
 
         }
+    }
+    static class ServerThread extends Thread{
+        private int serverPort;
+        public ServerThread(int serverPort){
+            this.serverPort = serverPort;
+
+        }
+        @Override
+        public void run() {
+            Socket socket = null;
+            try {
+                socket = new Socket("127.0.0.1", serverPort);
+
+            } catch (IOException e) {
+                System.out.println("connection fail");
+            }
+            DataOutputStream out = null;
+
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                out.writeUTF(NEW_ADD);
+            } catch (IOException e) {
+                System.out.println("OutPut Exception");
+            }
+
+        }
+
+
     }
 }
